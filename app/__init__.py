655518723fe32fdfdef3483bb48fd8febc85fe39
senakilinc <78450817+senakilinc@users.noreply.github.com>
2023-06-10 20:24:21 +0200

    Current version - uncombined with pattern matching.

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+#babel = Babel(app)
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['ALLOWED_EXTENSIONS'] = {'mp4', 'mov', 'avi', 'wmv', 'flv', 'webm'}
+app.config['UPLOAD_PATH'] = 'C:\\Users\\Sena\\BSP06-WEB\\venv\\app\\static\\uploads'
+app.config['STATISTICS_FOLDER'] = 'C:\\Users\\Sena\\BSP06-WEB\\venv\\app\\static\\videos'
+
+
+from app import routes, models, screenshot
